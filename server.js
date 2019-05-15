bf474c6f15c02e369920f5ff008e2001f386d4f0
Jordan Wick <wickj@mit.edu>
2019-05-15 02:09:04 -0400

    Added files from tutorial

---
+++ server.js
@@ -0,0 +1,14 @@
+//Requires
+const express = require('express');
+const app = express();
+const path = require('path');
+const chalk = require('chalk');
+const morgan = require('morgan');
+//Static Routes
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.use(morgan('dev')) // logging
+//Main App Route
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
+const port = 1337;
+//Run Server
+app.listen(process.env.PORT || port, () => console.log(chalk.blue(`Listening intently on port ${port}`)));
