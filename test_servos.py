f95473d87b8b2261f3383000ae1baf8aa220e05e
Jordan Wick <wickj@mit.edu>
2019-05-17 03:19:46 -0400

    Added servo test script

--- test_servos.py
+++ test_servos.py
@@ -1,6 +1,7 @@
 from adafruit_servokit import ServoKit
 from time import sleep
+import sys
+
+angle = int(sys.argv[1])
 kit = ServoKit(channels=16)
-kit.servo[0].angle = 0
-sleep(2)
-kit.servo[0].angle = 180
+kit.servo[0].angle = angle
