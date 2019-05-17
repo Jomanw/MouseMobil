c2e88513c541a6c9e1094a98b7b744e2e9c6e5bd
Jordan Wick <wickj@mit.edu>
2019-05-17 19:40:21 -0400

    updated servo file

--- test_servos.py
+++ test_servos.py
@@ -2,6 +2,12 @@ from adafruit_servokit import ServoKit
 from time import sleep
 import sys
 
-angle = int(sys.argv[1])
+# angle = int(sys.argv[1])
+# kit = ServoKit(channels=16)
+# kit.servo[0].angle = angle
+
 kit = ServoKit(channels=16)
-kit.servo[0].angle = angle
+throttle_1 = int(sys.argv[1])
+throttle_2 = int(sys.argv[2])
+kit.continuous_servo[0].throttle = throttle_1
+kit.continuous_servo[1].throttle = throttle_2
